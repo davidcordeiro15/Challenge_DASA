459e97e4225ac4529a5310a6a21238df1588b4fc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 21:36:54 -0300

    fix: Arrumando erro de cadastro

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,19 +2,20 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/TelaInicial.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/TelaInicial.fxml")));
         Scene scene = new Scene(root, 600, 400);
-        //scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         primaryStage.setTitle("BioMeasure");
         primaryStage.setScene(scene);
-        primaryStage.setMaximized(true); // tela cheia
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
