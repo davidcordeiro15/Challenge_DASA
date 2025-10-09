4ce43bee158cf439c07b3a323cf209d89cada8ca
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2025-10-08 23:42:59 -0300

    feat: Começando a API

---
+++ src/main/java/org/example/api/model/UserModel.java
@@ -0,0 +1,17 @@
+package org.example.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserModel {
+    @Id
+    private int id;
+
+    private String email;
+    private String senha;
+}
